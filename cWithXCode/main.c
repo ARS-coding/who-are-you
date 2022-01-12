962ad7df87bc51d031c526fb9ba2a97f7383aec5
Ali Rıza Şahin <riza.sahin@getir.com>
2022-01-12 12:51:38 +0300

    Initial Commit

---
+++ cWithXCode/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cWithXCode
+//
+//  Created by Ali Rıza Şahin on 12.01.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
